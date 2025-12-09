9f5bb9296de2be0b8e7736648283ce56a44f166b
Muhammad Ali <m.alibabar2004@gmail.com>
2025-12-09 05:28:24 -0800

    Added root route for live backend message

--- Backend/server.js
+++ Backend/server.js
@@ -11,6 +11,9 @@ dotenv.config();
 const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.get("/", (req, res) => {
+  res.send("Thrifti Backend is live!");
+});
 
 
 
